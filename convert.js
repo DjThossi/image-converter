933235b978c8798fa6b74714ed263b0573d5338e
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-11-26 19:33:21 +0100

    Move width and quality to top and reformat code

--- convert.js
+++ convert.js
@@ -3,20 +3,22 @@ var easyimg = require('easyimage');
 
 var inputFolder = './input/';
 var outputFolder = './output/';
+var width = 1140;
+var quality = 75;
 
-fs.readdir(inputFolder, function(err, files) {
-    files.forEach(function (fileName){
-        if (fileName.substr(0,1) === '.') {
+fs.readdir(inputFolder, function (err, files) {
+    files.forEach(function (fileName) {
+        if (fileName.substr(0, 1) === '.') {
             return;
         }
 
         easyimg.resize({
             src: inputFolder + fileName,
             dst: outputFolder + fileName,
-            width:1140,
-            quality: 75
+            width: width,
+            quality: quality
         }).then(
-            function(image) {
+            function (image) {
                 console.log('Resized ' + fileName + ' to ' + image.width + ' x ' + image.height);
             },
             function (err) {
